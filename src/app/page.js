c806b086a21d58897f1cb5861ca7c1c4b4463215
siddharthagupta3 <siddharthagupta617@gmail.com>
2026-08-14 15:11:35 +0530

    Update code

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
@@ -9,10 +8,6 @@ import Contact from '../components/Contact';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import Skills from '@/components/Skills';
-
-const ThreeBox = dynamic(() => import('../components/ThreeBox'), {
-  ssr: false,
-});
 
 export default function Home() {
   return (
@@ -25,7 +20,6 @@ export default function Home() {
       <Certificates />
       <Contact />
       <Footer />
-      <ThreeBox />
     </main>
   );
 }
