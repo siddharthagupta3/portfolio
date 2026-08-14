0d75bad48a26c3a9b0cfd683681a051c02d9016d
siddharthagupta3 <siddharthagupta617@gmail.com>
2026-08-14 16:35:23 +0530

    Remove Certifications & Achievements section from homepage

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
-import Certificates from '../components/Certificates';
 import Contact from '../components/Contact';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
@@ -17,7 +16,6 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      <Certificates />
       <Contact />
       <Footer />
     </main>
